321d8707a50ac8d75d28b470c831885fccf3aa7d
Kevin Martin <dev.kmartin@gmail.com>
2022-01-17 16:04:36 +0100

    Add pi and heroku environment

---
+++ src/environments/environment.prod-pi.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  parcelOrganizerApiUrl: 'https://api.parcelorganizer.kmartin.nl',
+  parcelOrganizerClientId: 'parcel-organizer-web',
+  parcelOrganizerClientSecret: 'secret',
+};
